c03a8d47caf893faf51864cafcb1b23d5d07aec3
zeyad <zeyad@yahoo.com>
2024-03-14 03:14:16 +0200

    finish canel page

--- lib/screens/appioment/cancel_page.dart
+++ lib/screens/appioment/cancel_page.dart
@@ -9,8 +9,8 @@ class CancelPage extends StatefulWidget {
   State<CancelPage> createState() => _CancelPageState();
 }
 
-class _CancelPageState extends State<CancelPage> { 
-  String groupValue = "yes";
+class _CancelPageState extends State<CancelPage> {
+  String groupValue = "Schedule Change";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,115 +19,208 @@ class _CancelPageState extends State<CancelPage> {
         centerTitle: true,
         title: const Text(
           "Cancel Booking",
-          style: TextStyle(fontSize: 18),
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            const SubtitleTextWidget(
-              label: "Please select the reason for cancellations:",
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Column(
-                children: [
-                  Radio(
-                    value: "Schedule Change",
-                    groupValue: groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        groupValue = value!;
-                      });
-                      const Text("Schedule Change");
-                    },
-                  ),
-                  Radio(
-                    value: "Weather conditions",
-                    groupValue: groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        groupValue = value!;
-                      });
-                      const Text("Weather conditions");
-                    },
-                  ),
-                  Radio(
-                    value: "Unexpected Work",
-                    groupValue: groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        groupValue = value!;
-                      });
-                      const Text("Unexpected Work");
-                    },
-                  ),
-                  Radio(
-                    value: "Childcare Issue",
-                    groupValue: groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        groupValue = value!;
-                      });
-                      const Text("Childcare Issue");
-                    },
-                  ),
-                  Radio(
-                    value: "Travel Delays",
-                    groupValue: groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        groupValue = value!;
-                      });
-                      const Text("Travel Delays");
-                    },
-                  ),
-                  Radio(
-                    value: "Other",
-                    groupValue: groupValue,
-                    onChanged: (value) {
-                      setState(() {
-                        groupValue = value!;
-                      });
-                      const Text("Other");
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const Divider(),
-                  TextFormField(
-                    key: const ValueKey("Description"),
-                    minLines: 5,
-                    maxLines: 10,
-                    maxLength: 1000,
-                    textCapitalization: TextCapitalization.sentences,
-                    decoration: const InputDecoration(
-                      hintText: "Enter Your reason",
-                    ),
-                    onTap: () {},
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Submit",
-                          style: TextStyle(
-                              color: Colors.black,
-                              backgroundColor: Colors.white),
-                        )),
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 20,
+            left: 15,
+            right: 15,
+          ),
+          child: Column(
+            children: [
+              const SubtitleTextWidget(
+                label: "Please select the reason for cancellations:",
+                fontWeight: FontWeight.bold,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 15,
+              ),
+              const Divider(),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Radio(
+                          value: "Schedule Change",
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValue = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Schedule Change",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "Weather conditions",
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValue = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Weather conditions",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "Unexpected Work",
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValue = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Unexpected Work",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "Childcare Issue",
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValue = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Childcare Issue",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "Travel Delays",
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValue = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Travel Delays",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "Other",
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            setState(() {
+                              groupValue = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Other",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Divider(),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      key: const ValueKey("Description"),
+                      minLines: 5,
+                      maxLines: 10,
+                      maxLength: 1000,
+                      textCapitalization: TextCapitalization.sentences,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Your reason",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(12.0),
+                          ),
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                          onPressed: () {
+                            //Navigator.pushNamed(context, CanceledSchedule.routeName);
+                          },
+                          child: const Text(
+                            "Submit",
+                            style: TextStyle(
+                                color: Colors.black,
+                                backgroundColor: Colors.white),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
